5931d1a63ac22d9b87795fb3e3e595666603164e
Dima4ka <dima4ka@me.com>
2015-02-22 20:14:09 -0500

    Initial Commit

---
+++ Personal Website/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Personal_Website.Startup))]
+namespace Personal_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
