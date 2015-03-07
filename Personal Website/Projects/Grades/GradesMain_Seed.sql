68217af54ec9355a8480952acc41ed77643256c1
Dima4ka <dima4ka@me.com>
2015-03-07 00:39:26 -0500

    Add basic front-end to grades project.

---
+++ Personal Website/Projects/Grades/GradesMain_Seed.sql
@@ -0,0 +1,7 @@
+﻿/* Commands to seed the values into the SimpleGrades TABLE */
+
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Molecularity',					'CH1010', 93.05,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Database Systems I',			'CS3431', 91.70,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Probability for Applications',	'MA2621', 90.40,	'A', 'in progress...'	);
+
+INSERT INTO SimpleGrades VALUES( 'B', 2014, 'IQP Term C',					'HH1423', 100,		'A', 'in progress...'	);
--- Personal Website/SQL Scripts/GradesMain_Seed.sql
+++
@@ -1,5 +0,0 @@
-﻿/* Commands to seed the values into the SimpleGrades TABLE */
-
-INSERT INTO SimpleGrades VALUES( 'C', 2014, 'Molecularity',					'CH1010', 93.05, 'A', 'Completed'		);
-INSERT INTO SimpleGrades VALUES( 'C', 2014, 'Database Systems I',			'CS3431', 91.70, 'A', 'Completed'		);
-INSERT INTO SimpleGrades VALUES( 'C', 2014, 'Probability for Applications',	'MA2621', 90.40, 'A', 'in progress...'	);
