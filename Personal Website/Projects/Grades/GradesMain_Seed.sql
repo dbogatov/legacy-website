831e36656be405bf053c7fcc558f955049509a6a
Dima4ka <dima4ka@me.com>
2015-03-07 19:22:13 -0500

    Add all grades to seed.

--- Personal Website/Projects/Grades/GradesMain_Seed.sql
+++ Personal Website/Projects/Grades/GradesMain_Seed.sql
@@ -1,7 +1,32 @@
 ﻿/* Commands to seed the values into the SimpleGrades TABLE */
 
-INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Molecularity',					'CH1010', 93.05,	'A', 'Completed'		);
-INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Database Systems I',			'CS3431', 91.70,	'A', 'Completed'		);
-INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Probability for Applications',	'MA2621', 90.40,	'A', 'in progress...'	);
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Molecularity',											'CH1010',	93.05,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Database Systems I',									'CS3431',	91.70,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'Probability for Applications',							'MA2621',	90.40,	'A', 'in progress...'	);
+INSERT INTO SimpleGrades VALUES( 'C', 2015, 'IQP Term C',											'HH1423',	100,	'A', 'in progress...'	);
 
-INSERT INTO SimpleGrades VALUES( 'B', 2014, 'IQP Term C',					'HH1423', 100,		'A', 'in progress...'	);
+INSERT INTO SimpleGrades VALUES( 'B', 2014, 'IQP Term B',											'HH1423',	100,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'B', 2014, 'Software Engineering',									'CS3733',	89.85,	'B', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'B', 2014, 'Digital Image',										'AR1101',	97.65,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'B', 2014, 'Volleyball & Squash',									'PE1002',	100,	'A', 'Completed'		);
+
+INSERT INTO SimpleGrades VALUES( 'A', 2014, 'ISP Trading Systems,  Investment & Risk Management',	'HH2000',	100,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'A', 2014, 'Intermediate Mechanics I',								'PH2201',	90.55,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'A', 2014, 'Applied Statistics I',									'MA2611',	95.72,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'A', 2014, 'Discrete Mathematics',									'CS2202',	93.5,	'A', 'Completed'		);
+
+INSERT INTO SimpleGrades VALUES( 'D', 2014, 'Macroeconomics',										'ECON1120',	93.33,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'D', 2014, 'Machine Organization and Assembly Language',			'CS2011',	95.6,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'D', 2014, 'Ordinary Differential Equations',						'MA2051',	98.5,	'A', 'Completed'		);
+
+INSERT INTO SimpleGrades VALUES( 'C', 2014, 'Calculus IV',											'MA1024',	96.02,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'C', 2014, 'Essentials of Art',									'AR1100',	96.7,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'C', 2014, 'Systems Programming',									'CS2303',	94.4,	'A', 'Completed'		);
+
+INSERT INTO SimpleGrades VALUES( 'B', 2013, 'Calculus III',											'MA1023',	95.7,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'B', 2013, 'General Electricity',									'PH1120',	96,		'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'B', 2013, 'Introduction to Object Oriented Programming',			'CS2102',	91.4,	'A', 'Completed'		);
+										 
+INSERT INTO SimpleGrades VALUES( 'A', 2013, 'Calculus II',											'MA1022',	97.4,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'A', 2013, 'General Mechanics',									'PH1110',	95.3,	'A', 'Completed'		);
+INSERT INTO SimpleGrades VALUES( 'A', 2013, 'Introduction to Program Design',						'CS1101',	97.6,	'A', 'Completed'		);
