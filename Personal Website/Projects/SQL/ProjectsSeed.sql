ac2f7a3b034274bb276382ae7bcb807aff488b2d
Dima4ka <dima4ka@me.com>
2015-04-05 01:04:06 -0400

    Add parser to projects DB.

--- Personal Website/Projects/SQL/ProjectsSeed.sql
+++ Personal Website/Projects/SQL/ProjectsSeed.sql
@@ -2,6 +2,7 @@
 INSERT INTO Projects VALUES (2, 'Personal Website',	'Personal Website is a private project containing the resume, projects and blog of its author.',	SYSDATETIME(), 'http://dbogatov.org', 'https://bitbucket.org/Dima4ka/personal-website', '../Images/PersonalWebsite.png');
 INSERT INTO Projects VALUES (3, 'Banker Game Assistant',	'This is an iOS application serving as a banker for board games. It keeps track of all your money accounts.',	'20150215', 'Banker/Default.aspx', 'https://bitbucket.org/Dima4ka/monopoly-banker/', '../Images/Banker.png');
 INSERT INTO Projects VALUES (4, 'WPI Event Creator',	'This is an iOS application allowing WPI students to create appointment events with faculty easily and quickly.',	'20140410', 'Calendar-Event-Creator/Default.aspx', 'https://bitbucket.org/Dima4ka/wpi-calendar-event-creator/', '../Images/WPICalendarEventCreator.png');
+INSERT INTO Projects VALUES (5, 'Finance Parser',	'Simple parser uses Google API to display current bid and ask prices for selected symbols and strikes.',	'20150405', 'Google-Finance-Parser/Default.aspx', 'https://bitbucket.org/Dima4ka/personal-website', '../Images/FinanceParser.png');
 
 
 INSERT INTO Tags VALUES (1, 'Large');
