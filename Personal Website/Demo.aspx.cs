6af6717ea45d1bf317b0c6f70b3509e7f9f6d507
Dima4ka <dima4ka@me.com>
2015-04-29 02:13:33 -0400

    Add demo. and clear. subdomains.

---
+++ Personal Website/Demo.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Personal_Website.Models;
+
+namespace Personal_Website {
+	public partial class Demo : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			if (!Authentication.isDemo()) {
+				Authentication.startDemo();
+			} else {
+				Response.Redirect("~/");
+			}
+		}
+	}
+}
