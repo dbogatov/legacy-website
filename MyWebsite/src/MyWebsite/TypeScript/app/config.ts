97c4e34d22728af8ec50bde93983314fc737f87f
Dmytro <dbogatov@wpi.edu>
2016-03-20 01:15:03 -0400

    Fix TS usage.

--- MyWebsite/src/MyWebsite/TypeScript/app/config.ts
+++ MyWebsite/src/MyWebsite/TypeScript/app/config.ts
@@ -10,32 +10,3 @@ require(['MasterPage'], (MasterPage) => {
 	var app = new MasterPage();
 	app.run();
 });
-
-declare var globalModule: string;
-
-switch (globalModule) {
-	case "ProjectPage":
-		require(['ProjectPage'], (ProjectPage) => {
-			var app = new ProjectPage();
-			app.run();
-		});
-		break;
-
-	case "CoursesPage":
-		require(['CoursesPage'], (CoursesPage) => {
-			var app = new CoursesPage();
-			app.run();
-		});
-		break;
-
-	case "ContactPage":
-		require(['ContactPage'], (ContactPage) => {
-			var app = new ContactPage();
-			app.run();
-		});
-		break;
-
-	default:
-		break;
-}
-
