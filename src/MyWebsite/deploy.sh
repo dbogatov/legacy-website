b9b0a32bcdfb64f5826abe94e4e98d3458e1148c
Dima4ka <dbogatov@wpi.edu>
2016-04-09 16:46:06 -0400

    Fix

--- src/MyWebsite/deploy.sh
+++ src/MyWebsite/deploy.sh
@@ -3,24 +3,26 @@
 set -u
 set -e
 
+RUNTIME=$1
+WEBSITEPATH=$2 # eq. "/var/aspnet/mywebsite"
+SUPERVISORJOB=$3 # eq. "mywebsite"
+DIR=$4
+
 # CD to proper directory
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#SOURCE="${BASH_SOURCE[0]}"
+#while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+#  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#  SOURCE="$(readlink "$SOURCE")"
+#  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+#done
+#DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 cd $DIR
 
 printf "Current dir "$DIR"\n"
 
 # Now in good dir
-
-WEBSITEPATH="/var/aspnet/mywebsite"
-SUPERVISORJOB="mywebsite"
 
 git fetch --all --quiet
 
@@ -31,12 +33,12 @@ git fetch --all --quiet
 
     # Git
     printf "Git fetch/reset/pull\n"
-    git reset --quiet --hard origin/master
-    git pull --quiet
+#    git reset --quiet --hard origin/master
+#    git pull --quiet
 
     # Supervisor stops site
     printf "Stop website\n"
-    supervisorctl stop mywebsite
+    supervisorctl stop $SUPERVISORJOB
 
     # Clean directory
     printf "CLeaning directory\n"
@@ -51,18 +53,18 @@ git fetch --all --quiet
 
     # know bug: dnu tarcks elapsed time which does not work with bash
     set +e
-    dnu publish --out /var/aspnet/mywebsite
+    dnu publish --out $WEBSITEPATH/
     set -e
 
     # Fix runtime
     printf "Fixing runtime\n"
-    perl -pi -e 's/"dnx"/"\/root\/.dnx\/runtimes\/dnx-coreclr-linux-x64.1.0.0-rc1-update1\/bin\/dnx"/g' /var/aspnet/mywebsite/approot/web
+    perl -pi -e 's/"dnx"/"'$RUNTIME'\/dnx"/g' $WEBSITEPATH/approot/web
 
     # Supervisor uns website
     printf "Run website\n"
-    supervisorctl start mywebsite
+    supervisorctl start $SUPERVISORJOB
     
-    /root/text.sh send --text="Deploy for mywebsite complete." --phones=15086677440
+    # /root/text.sh send --text="Deploy for mywebsite complete." --phones=15086677440
 
     printf "Deploy done.\n"
 
